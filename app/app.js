a88a5f8571aaf9aaa72034bdc36fe59a6483ffa7
Mstanton98 <mstanton98@gmail.com>
2016-11-08 14:54:16 -0800

    added service and views

--- app/app.js
+++ app/app.js
@@ -1,20 +1,34 @@
 import angular from 'angular';
 import angularMaterialize from 'angular-materialize';
+import uiRouter from 'angular-ui-router';
 
 import CatalogCtrl from './catalog/catalog.controller';
+import CatalogService from './catalog/catalog.service';
 
 import CartCtrl from './cart/cart.controller';
 import CartService from './cart/cart.service';
 
-angular.module('my-app', [angularMaterialize])
+angular.module('my-app', [angularMaterialize, uiRouter])
+  .service('catalogService', CatalogService)
   .service('cartService', CartService)
   .controller('CatalogCtrl', CatalogCtrl)
-  .controller('CartCtrl', CartCtrl);
-
-$(".dropdown-button").dropdown();
-
-$('.modal-trigger').leanModal({
-    dismissible: true,
-    in_duration: 300,
-    out_duration: 200
-  });
+  .controller('CartCtrl', CartCtrl)
+  .config(['$stateProvider', ($stateProvider) => {
+    $stateProvider
+      .state('index', {
+        url: '',
+        templateUrl: 'views/catalog.html'
+      })
+      .state('checkout', {
+        url: '/checkout',
+        templateUrl: 'views/checkout.html'
+      })
+      .state('orders', {
+        url: '/orders',
+        templateUrl: 'views/orders.html'
+      })
+      .state('login', {
+        url: '/login',
+        templateUrl: 'views/login.html'
+      });
+  }]);
