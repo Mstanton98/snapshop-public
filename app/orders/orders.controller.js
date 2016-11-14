ccf23d459c924e45bbdfd58f64286499d67a6660
Mstanton98 <mstanton98@gmail.com>
2016-11-14 09:32:32 -0800

    added order item controller

--- app/orders/orders.controller.js
+++ app/orders/orders.controller.js
@@ -2,10 +2,19 @@ class OrderCtrl {
   constructor(orderSvc) {
     this.orderSvc = orderSvc;
     this.orders;
+    this.items;
 
     this.orderSvc.getUserOrders()
       .then((res) => {
         this.orders = res;
+      })
+      .catch((err) => console.log(err));
+  }
+
+  getUserItems(orderId) {
+    return this.orderSvc.getUserOrderItems(orderId)
+      .then((res) => {
+        this.items = res;
       })
       .catch((err) => console.log(err));
   }
--- app/orders/orders.service.js
+++ app/orders/orders.service.js
@@ -13,6 +13,17 @@ class OrderService {
       })
 
   }
+
+  getUserOrderItems(orderId) {
+    return this.$http.get(`/api/orders/${orderId}`)
+      .then((res) => {
+        console.log(res.data.items);
+        return res.data.items;
+      })
+      .catch((err) => {
+        console.error(err);
+      })
+  }
 }
 
 OrderService.$inject = ['$http'];
